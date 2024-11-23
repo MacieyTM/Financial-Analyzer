d3a24eb0611b521a96e6150344a1a413ade15926
MacieyTM <mghej3@wp.pl>
2024-11-23 03:41:45 +0100

    left and right pages added

---
+++ src/app/left/left.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LeftPage } from './left.page';
+
+describe('LeftPage', () => {
+  let component: LeftPage;
+  let fixture: ComponentFixture<LeftPage>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LeftPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
