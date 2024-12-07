974270f5b39de5aa72a7c121dbde3252dcc286db
MacieyTM <mghej3@wp.pl>
2024-12-07 21:09:18 +0100

    change detection added

--- src/app/home/pages/manage-account/manage-account.page.ts
+++ src/app/home/pages/manage-account/manage-account.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 
 @Component({
 	selector: "app-manage-account",
 	templateUrl: "./manage-account.page.html",
 	styleUrls: ["./manage-account.page.scss"],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ManageAccountPage implements OnInit {
 	constructor() {}
