2542ce09bc84bb5289ee23ecec50f9bcf073ef5f
MacieyTM <mghej3@wp.pl>
2025-10-11 01:28:08 +0200

    logic added

--- src/app/home/pages/kpi-trends/kpi-trends.module.ts
+++ src/app/home/pages/kpi-trends/kpi-trends.module.ts
@@ -7,9 +7,10 @@ import { IonicModule } from "@ionic/angular";
 import { KpiTrendsPageRoutingModule } from "./kpi-trends-routing.module";
 
 import { KpiTrendsPage } from "./kpi-trends.page";
+import { UiModule } from "src/app/ui.module";
 
 @NgModule({
-	imports: [CommonModule, FormsModule, IonicModule, KpiTrendsPageRoutingModule],
+	imports: [CommonModule, FormsModule, IonicModule, KpiTrendsPageRoutingModule, UiModule],
 	declarations: [KpiTrendsPage],
 })
 export class KpiTrendsPageModule {}
