75be35472ca7793a352ee79b1dc37e11b065f84f
MacieyTM <mghej3@wp.pl>
2025-10-11 16:16:41 +0200

    wip

--- src/app/home/pages/manage-account/pages/user-settings/user-settings.page.ts
+++ src/app/home/pages/manage-account/pages/user-settings/user-settings.page.ts
@@ -8,23 +8,32 @@ import { NavController } from "@ionic/angular";
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserSettingsPage implements OnInit {
-	public userName: string = "";
-	public userSurname: string = "";
+	public userName!: string;
+	public userSurname!: string;
 
 	constructor(private readonly navController: NavController) {}
 
 	ngOnInit() {
-		// this.userName = localStorage.getItem("userFullName")?.split("");
-		// this.userName = localStorage.getItem("userFullName");
-		// if (this.userName) {
-		// 	localStorage.setItem("userFullName", this.userName);
-		// } else {
-		// 	localStorage.getItem("userFullName");
-		// }
+		this.userName = "";
+		this.userSurname = "";
+		const storedFullName = localStorage.getItem("userFullName");
+
+		if (storedFullName) {
+			const [name, surName] = storedFullName.split(" ");
+
+			this.userName = name || "";
+			this.userSurname = surName || "";
+		}
 	}
 
 	protected save(name: string, surname: string) {
-		const fullUserName = name + " " + surname;
+		if (!name || !surname) {
+			alert("Both fields are required.");
+			return;
+		}
+
+		const fullUserName = `${name} ${surname}`;
+
 		localStorage.setItem("userFullName", fullUserName);
 		this.navController.back();
 	}
