84ef6570a6328a5ae7b66891aa36c10a8e1ed032
MacieyTM <mghej3@wp.pl>
2025-10-14 22:57:34 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,24 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { Component } from "@angular/core";
 import { ScreenOrientation, OrientationType } from "@capawesome/capacitor-screen-orientation";
 import { Platform } from "@ionic/angular";
-// import { Capacitor } from "@capacitor/core";
+import { Capacitor } from "@capacitor/core";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "app.component.html",
 	styleUrls: ["app.component.scss"],
-	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-	constructor(private readonly platform: Platform) {
-		if (this.platform.is("android") || this.platform.is("ios")) {
-			ScreenOrientation.lock({ type: OrientationType.PORTRAIT });
+	constructor(private readonly platform: Platform) {}
+
+	public async ngOnInit(): Promise<void> {
+		await this.platform.ready();
+
+		if (Capacitor.getPlatform() !== "web") {
+			await ScreenOrientation.lock({ type: OrientationType.PORTRAIT });
 		}
-		// if (Capacitor.getPlatform() !== "web") {
-		// 	ScreenOrientation.lock({ type: OrientationType.PORTRAIT });
+		// if (this.platform.is("android") || this.platform.is("ios")) {
+		// 	await ScreenOrientation.lock({ type: OrientationType.PORTRAIT });
 		// }
 	}
 }
