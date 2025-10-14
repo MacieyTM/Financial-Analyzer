5c5b5f990532aebe97256aedb4c3dec5ae50ae72
MacieyTM <mghej3@wp.pl>
2025-10-14 16:11:02 +0200

    wip

--- src/app/home/pages/kpi-trends/kpi-trends.page.ts
+++ src/app/home/pages/kpi-trends/kpi-trends.page.ts
@@ -1,4 +1,20 @@
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { ChartOptions, ChartData } from "chart.js";
+
+const MONTHS: string[] = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December",
+];
 
 @Component({
 	selector: "app-kpi-trends",
@@ -7,6 +23,22 @@ import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class KpiTrendsPage implements OnInit {
+	public chartOptions: ChartOptions = {
+		responsive: true,
+	};
+
+	public chartData: ChartData = {
+		labels: MONTHS,
+		datasets: [
+			{
+				fill: true,
+				label: "Money Amount",
+				borderColor: "orange",
+				data: [65, 59, 80, 81, 56, 59, 80, 81, 56, 59, 80, 81],
+			},
+		],
+	};
+
 	constructor() {}
 
 	ngOnInit() {}
