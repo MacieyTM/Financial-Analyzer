2fa65524d74afc16bdfda82113975f69a0ab92ef
MacieyTM <mghej3@wp.pl>
2025-10-27 15:02:16 +0100

    wip

--- src/app/home/pages/kpi-trends/add-kpi-values/add-kpi-values.module.ts
+++ src/app/home/pages/kpi-trends/add-kpi-values/add-kpi-values.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from "@angular/forms";
 import { IonicModule } from "@ionic/angular";
 import { AddKpiValuesPageRoutingModule } from "./add-kpi-values-routing.module";
 import { AddKpiValuesPage } from "./add-kpi-values.page";
+import { UiModule } from "src/app/ui.module";
 
 @NgModule({
-	imports: [CommonModule, FormsModule, IonicModule, AddKpiValuesPageRoutingModule],
+	imports: [CommonModule, FormsModule, IonicModule, AddKpiValuesPageRoutingModule, UiModule],
 	declarations: [AddKpiValuesPage],
 })
 export class AddKpiValuesPageModule {}
--- src/app/home/pages/kpi-trends/add-kpi-values/add-kpi-values.page.ts
+++ src/app/home/pages/kpi-trends/add-kpi-values/add-kpi-values.page.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 
 @Component({
 	selector: "app-add-kpi-values",
 	templateUrl: "./add-kpi-values.page.html",
 	styleUrls: ["./add-kpi-values.page.scss"],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddKpiValuesPage implements OnInit {
+	protected readonly kpTypesOptions = [
+		{ value: "line", label: "line" },
+		{ value: "bar", label: "bar" },
+		{ value: "doughnut", label: "doughnut" },
+	];
+
 	constructor() {}
 
 	ngOnInit() {}
