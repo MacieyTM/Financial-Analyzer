19ce1918f22b48f24ffccac839910529f5f27b80
MacieyTM <mghej3@wp.pl>
2025-10-28 22:53:27 +0100

    fix

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -74,21 +74,17 @@ export class PhotoService {
 	}
 
 	public async deletePicture(photo: UserPhoto, position: number): Promise<void> {
-		try {
-			await Filesystem.deleteFile({
-				path: photo.filepath,
-				directory: Directory.Data,
-			});
+		await Filesystem.deleteFile({
+			path: photo.filepath,
+			directory: Directory.Data,
+		});
 
-			this.photos.splice(position, 1);
+		this.photos.splice(position, 1);
 
-			await Preferences.set({
-				key: this.PHOTO_STORAGE,
-				value: JSON.stringify(this.photos),
-			});
-		} catch (error) {
-			console.log("Error deleting photo:", error);
-		}
+		await Preferences.set({
+			key: this.PHOTO_STORAGE,
+			value: JSON.stringify(this.photos),
+		});
 	}
 
 	private async readAsBase64(photo: Photo): Promise<string | Blob> {
