d1cf2f5c7372a9ba0228679390799b5c99586f5b
MacieyTM <mghej3@wp.pl>
2025-12-23 16:14:04 +0100

    dynamic quarter vlaues added to dashboard view

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,18 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from "@angular/core";
-import { CHART_DATA_QUARTERS, CHART_LABEL_QUARTERS } from "src/app/models/chart.model";
+import {
+	ChangeDetectionStrategy,
+	ChangeDetectorRef,
+	Component,
+	OnInit,
+	signal,
+} from "@angular/core";
+import { CHART_LABEL_MONTHS, CHART_LABEL_QUARTERS } from "src/app/models/chart.model";
 import { PhotoService } from "../services/photo.service";
 import { ChartConfiguration } from "chart.js";
+import { KpiEntry } from "./pages/kpi-trends/kpi-trends.page";
+
+export const BORDER_COLOR = getComputedStyle(document.documentElement).getPropertyValue(
+	"--ion-color-primary"
+);
 
 // const BANK_ACCOUNT_AMOUNT: number = 1234567.89;
 const BANK_ACCOUNT_AMOUNT: number = +localStorage.getItem("selectedMoney");
@@ -19,22 +30,21 @@ export class HomePage implements OnInit {
 	protected chartOptions: ChartConfiguration<"doughnut">["options"];
 
 	private data: number[];
-	private borderColor: string;
 
-	constructor(protected readonly photoService: PhotoService) {}
+	constructor(
+		protected readonly photoService: PhotoService,
+		private readonly cdr: ChangeDetectorRef
+	) {}
 
 	public ngOnInit(): void {
-		this.data = CHART_DATA_QUARTERS;
-		this.borderColor = getComputedStyle(document.documentElement).getPropertyValue(
-			"--ion-color-primary"
-		);
+		this.data = this.calculateQuarterData();
 
 		this.chartData = {
 			labels: CHART_LABEL_QUARTERS,
 			datasets: [
 				{
 					data: this.data,
-					borderColor: this.borderColor,
+					borderColor: BORDER_COLOR,
 
 					backgroundColor: ["#f00", "#ff0", "#0f0", "#00f"],
 					hoverBackgroundColor: ["#f00", "#ff0", "#0f0", "#00f"],
@@ -60,6 +70,8 @@ export class HomePage implements OnInit {
 				},
 			},
 		};
+
+		this.cdr.detectChanges();
 	}
 
 	protected addPhotoToStorage(): void {
@@ -68,5 +80,27 @@ export class HomePage implements OnInit {
 
 	protected toggleHelp(): void {
 		this.helpVisible.update((prev) => !prev);
+	}
+
+	private calculateQuarterData(): number[] {
+		const storedKpiData: KpiEntry[] = JSON.parse(localStorage.getItem("kpiData")) || [];
+		const monthIndexMap: Record<string, number> = {};
+
+		CHART_LABEL_MONTHS.forEach((month, i) => (monthIndexMap[month.toLowerCase()] = i));
+
+		const quarters = [0, 0, 0, 0];
+
+		storedKpiData.forEach((entry) => {
+			const monthIdx = monthIndexMap[entry.month.toLowerCase()];
+
+			if (monthIdx !== undefined) {
+				const quarter = Math.floor(monthIdx / 3);
+				quarters[quarter] += entry.money;
+			}
+		});
+
+		console.log("Calculated Quarters:", quarters);
+
+		return quarters;
 	}
 }
